fc1886b23da7ee8218711d8a4f8a9a4ca6abd000
collnell <cnell@usgs.gov>
2021-07-05 17:59:22 -0500

    add county public supply

--- src/fetch_wu.R
+++ src/fetch_wu.R
@@ -25,15 +25,15 @@ wu_vars <- wu_states[,c(2:4)]
 colnames(wu_vars) <- c('state_name', 'year', 'population_1000')
 
 # filter to withdrawal total variables - includes overall total and by saline / fresh & broken down by gw/sw
-wu_keep <- wu_states %>% select(contains('Mgal') & contains('withdrawals') & contains('total'),
+wu_keep <- wu_states %>% select(state_name, year, contains('Mgal') & contains('withdrawals') & contains('total'),
                                 -contains("Hydroelectric"), -contains("Fossil"), -contains("Geothermal"),-contains("Nuclear"), 
                                 -contains("Closed"), -contains("Once"), -contains('Deliveries'))  # drop subcategories in thermoelectric and irrigation
 
 # combine with state and year variables, relassify water use categories
 wu_clean <- wu_vars %>%
-  cbind(wu_keep) %>%
-  mutate(across(where(is.double) | where(is.character) & !c(state_cd, state_name, year), ~as.numeric(.x))) %>%
-  pivot_longer(!c(state_cd, state_name, year), "use", "use_value") %>%
+  left_join(wu_keep) %>%
+  mutate(across(where(is.double) | where(is.character) & !c(state_name, year), ~as.numeric(.x))) %>%
+  pivot_longer(!c(state_name, year), "use", "use_value") %>%
   mutate(used = use, use = str_split(tolower(use), '\\.')) %>%
   rowwise() %>%
   mutate(water_source = case_when(
@@ -107,7 +107,7 @@ ps_clean <- wu_states[,c(1:3)] %>%
   water_use =  'public supply') %>%
   ungroup()%>%
   dcast(state_name+year+water_use~used, value.var = "value")
-ps_clean %>% str
+ps_clean 
 
 colnames(ps_clean) <- c("state_name", "year", "water_use", "ps_facilities", "ps_gallons_person_day", "ps_pop_gw_1000", "ps_pop_sw_1000",
                         "ps_use_loss", "ps_reclaimed", "ps_gw_fresh", "ps_gw_saline", "ps_sw_fresh", "ps_sw_saline", "ps_pop_served_1000", "ps_fresh", "ps_gw","ps_saline","ps_sw","ps_total")
@@ -132,15 +132,15 @@ wu_vars_county <- wu_county[,c(2:6)]
 colnames(wu_vars_county) <- c('state_name', 'county_cd','county_nm', 'year', 'population_1000')
 
 # filter to withdrawal total variables - includes overall total and by saline / fresh & broken down by gw/sw
-wu_keep_county <- wu_county %>% select(contains('Mgal') & contains('withdrawals') & contains('total'),
+wu_keep_county <- wu_county %>% select(state_name, county_cd, county_nm, year, contains('Mgal') & contains('withdrawals') & contains('total'),
                                 -contains("Hydroelectric"), -contains("Fossil"), -contains("Geothermal"),-contains("Nuclear"), 
                                 -contains("Closed"), -contains("Once"), -contains('Deliveries'))  # drop subcategories in thermoelectric and irrigation
 wu_keep_county
 
 # combine with state and year variables, relassify water use categories
 wu_clean_county <- wu_vars_county %>%
-  cbind(wu_keep_county) %>%
-  mutate(across(where(is.double) | where(is.character), ~as.numeric(.x))) #%>%
+  left_join(wu_keep_county) %>%
+  mutate(across(where(is.double) | where(is.character) & !c(state_name, county_cd, county_nm, year), ~as.numeric(.x))) %>%
   pivot_longer(!c(state_name, county_cd, county_nm, year, population_1000), "use", "use_value") %>%
   mutate(used = use, use = str_split(tolower(use), '\\.')) %>%
   rowwise() %>%
@@ -166,3 +166,63 @@ wu_clean_county <- wu_vars_county %>%
   )
   ) %>%
   ungroup()
+
+wu_clean_county
+
+# create new variables based on type of water for surface water, groundwater, saline, and freshwater
+wu_clean_time_county <- wu_clean_county %>% 
+  filter(!is.na(water_use)) %>%
+  mutate(water_kind = case_when(
+    water_source == "total" & water_type == "both" ~ 'total',
+    water_source == "total" & water_type == "fresh" ~ 'fresh',
+    water_source == "total" & water_type == "saline" ~ 'saline',
+    water_source == "gw" & water_type == "both" ~ 'gw',
+    water_source == "sw" & water_type == "both" ~ 'sw'
+  )) %>% 
+  select(state_name, year, water_use, water_kind, value) %>%
+  filter(!is.na(water_kind)) %>%
+  group_by(state_name, year, water_use, water_kind) %>%
+  summarize(value = sum(value, na.rm = TRUE)) %>%
+  dcast(state_name + year + water_use ~ water_kind, value.var = "value") %>%
+  left_join(wu_vars_county)
+wu_clean_time_county
+
+# export county-level timeseries of water use categories by type/source
+# # using multiple total columns to account for differences in reporting
+wu_clean_time_county %>% 
+  select(state_name, year, water_use, sw, gw, saline, fresh, total) %>%
+  mutate(total_type = saline+fresh, total_source = gw+sw) %>%
+  write_csv("/Users/cnell/Documents/Projects/water-use-timeseries/out/use_county_time.csv")
+
+##
+## generate timeseries data on population trend and public supply
+ps_keep <- wu_county %>% select(contains("Public.Supply"), -contains("deliveries"))
+
+ps_clean <- wu_county[,c(1:5)] %>%
+  cbind(ps_keep) %>% 
+  mutate(across(where(is.double) | where(is.character) & !c(state_cd, county_cd, county_nm, state_name, year), ~as.numeric(.x))) %>%
+  pivot_longer(!c(state_cd, county_cd, county_nm, state_name, year), "use", "use_value") %>% 
+  mutate(used = use, use = str_split(tolower(use), '\\.')) %>% 
+  rowwise() %>%
+  mutate(water_source = case_when(
+    'groundwater' %in% unlist(use) ~ 'gw',
+    'surface' %in% unlist(use) ~ 'sw',
+    TRUE ~ 'total'
+  ),
+  water_type = case_when(
+    'saline' %in% unlist(use) ~ 'saline',
+    'fresh' %in% unlist(use) ~ 'fresh',
+    TRUE ~ 'both'
+  ),
+  water_use =  'public supply') %>%
+  ungroup()%>%
+  dcast(state_cd+state_name+county_nm+county_cd+year+water_use~used, value.var = "value")
+
+colnames(ps_clean) <- c("state_cd", "state_name","county_nm", "county_cd", "year", "water_use", "ps_facilities", "ps_gallons_person_day", "ps_pop_gw_1000", "ps_pop_sw_1000",
+                        "ps_use_loss", "ps_reclaimed", "ps_gw_fresh", "ps_gw_saline", "ps_sw_fresh", "ps_sw_saline", "ps_pop_served_1000", "ps_fresh", "ps_gw","ps_saline","ps_sw","ps_total")
+
+# reduce variables
+ps_clean %>% 
+  select(state_name:water_use, ps_gallons_person_day:ps_pop_sw_1000, ps_reclaimed, ps_pop_served_1000:ps_total) %>%
+  left_join(wu_vars) %>%
+  write_csv("out/use_publicsupply.csv")
